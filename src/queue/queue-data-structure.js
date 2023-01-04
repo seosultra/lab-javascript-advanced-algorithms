6b00810bfb442973caea613f10cc0deb5cb8a3cc
Sumia EOs <seosultra@gmail.com>
2023-01-04 23:29:20 +0100

    ”dome”

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -6,24 +6,40 @@ class Queue {
 
   canEnqueue() {
     // ... your code goes here
+    if (this.queueControl.length !== this.MAX_SIZE) return true;
+    return false;
   }
 
   isEmpty() {
     // ... your code goes here
+    if (this.queueControl.length !== 0) return false;
+    return true;
   }
 
   enqueue(item) {
     // ... your code goes here
+    if (this.canEnqueue()) {
+      this.queueControl.unshift(item);
+      return this.queueControl;
+    } else {
+      throw new Error("QUEUE_OVERFLOW");
+    }
   }
 
   dequeue() {
     // ... your code goes here
+    if (!this.isEmpty()) {
+      return this.queueControl.pop();
+    } else {
+      throw new Error("QUEUE_UNDERFLOW");
+    }
   }
 
   display() {
     // ... your code goes here
-  }  
+    return this.queueControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Queue;
+if (typeof module !== "undefined") module.exports = Queue;
--- src/queue/queue-dom.js
+++ src/queue/queue-dom.js
@@ -1,47 +1,80 @@
-const queueUL = document.querySelector('.list-queue');
-const queueInput = document.querySelector('.queue-input');
-const warningTopQueue = document.querySelector('#queue-container .warning-top');
+const queueUL = document.querySelector(".list-queue");
+const queueInput = document.querySelector(".queue-input");
+const warningTopQueue = document.querySelector("#queue-container .warning-top");
 const warningBottomQueue = document.querySelector(
-  '#queue-container .warning-bottom'
+  "#queue-container .warning-bottom"
 );
-const addQueue = document.querySelector('.btn-add-queue');
-const dequeue = document.querySelector('.btn-take-dequeue');
+const addQueue = document.querySelector(".btn-add-queue");
+const dequeue = document.querySelector(".btn-take-dequeue");
 
 const queue = new Queue();
 
 const clearQueueInput = () => {
   // ... your code goes here
+  queueInput.value = "";
 };
 
 const generateListQueue = () => {
   // ... your code goes here
+  warningBottomQueue.style.display = "none";
+  warningTopQueue.style.display = "none";
+  queueUL.innerHTML = "";
+  let qLength = queue.display().length;
+  let qSize = queue.MAX_SIZE - qLength;
+  queue.display().forEach((item) => {
+    let li = document.createElement("li");
+    li.className = "active";
+    li.innerText = item;
+    queueUL.appendChild(li);
+  });
+  for (let i = 0; i < qSize; i++) {
+    let li = document.createElement("li");
+    li.style.minWidth = "42px";
+    li.style.minHeight = "64px";
+    li.className = "inactive";
+    li.innerHTML = "&nbsp;";
+    queueUL.appendChild(li);
+  }
 };
 
 generateListQueue();
 
 const generateWarningQueue = (type) => {
-  if (type === 'underflow') {
+  if (type === "underflow") {
     // ... your code goes here
-  } else if (type === 'overflow') {
+    warningBottomQueue.style.display = "block";
+    warningBottomQueue.innerText = "⚠️ Queue underflow ⚠️";
+  } else if (type === "overflow") {
     // ... your code goes here
+    warningTopQueue.style.display = "block";
+    warningTopQueue.innerText = "⚠️ Queue overflow ⚠️";
   }
 };
 
 const addToQueue = () => {
   try {
     // ... your code goes here
+    queue.enqueue(queueInput.value);
+    generateListQueue();
   } catch (error) {
+    generateWarningQueue("overflow");
     // there was an overflow error, handle it
+    //alert(error.message);
+    clearQueueInput();
   }
 };
 
 const removeFromQueue = () => {
   try {
     // ... your code goes here
+    queue.dequeue();
+    generateListQueue();
   } catch (error) {
     // there was an underflow error, handle it
+    generateWarningQueue("underflow");
+    //alert(error.message);
   }
 };
 
-addQueue.addEventListener('click', addToQueue);
-dequeue.addEventListener('click', removeFromQueue);
+addQueue.addEventListener("click", addToQueue);
+dequeue.addEventListener("click", removeFromQueue);
